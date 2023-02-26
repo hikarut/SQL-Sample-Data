791bbb388166a80df5a9571a48888c0dde356dd2
hikarut <ht.hikaru.takahashi@gmail.com>
2023-02-26 10:04:52 +0900

    [update] SQL file

--- SQL/chapter06/SQL_answer_6-2.sql
+++ SQL/chapter06/SQL_answer_6-2.sql
@@ -3,10 +3,10 @@ SELECT
     SUM(p.price) AS total_price,
     COUNT(*) AS total_orders
 FROM
-    orders o
-    LEFT JOIN products p ON o.order_product_id = p.product_id
+    orders AS o
+    LEFT JOIN products AS p ON o.order_product_id = p.product_id
 WHERE
-	o.is_canceled = 0
+    o.is_canceled = 0
 GROUP BY
     o.user_id
 ORDER BY
